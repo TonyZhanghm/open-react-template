fd60d17b0ca79fffb616797c1f77317d0770498d
Tony Zhang <tonyzhang@Tonys-MacBook-Pro.local>
2024-05-15 17:14:56 -0700

    PostHog

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import { Inter, Architects_Daughter } from 'next/font/google'
 import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
 
+import { PHProvider } from './providers'
+import dynamic from 'next/dynamic'
+
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+  ssr: false,
+})
+
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
@@ -30,13 +37,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
-        <div className="flex flex-col min-h-screen overflow-hidden">
-          <Header />
-          {children}
-          <Banner />
-        </div>
-      </body>
+      <PHProvider>
+        <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
+          <div className="flex flex-col min-h-screen overflow-hidden">
+            <Header />
+            <PostHogPageView /> 
+            {children}
+            <Banner />
+          </div>
+        </body>
+      </PHProvider>
+
     </html>
   )
 }
